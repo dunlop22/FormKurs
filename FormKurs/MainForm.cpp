a4aad2b80d5b051ff85fd4dbfa8440914f4deb3a
Пользователь <20020110ll@gmail.com>
2023-04-02 14:54:23 +0700

    Start project

---
+++ FormKurs/MainForm.cpp
@@ -0,0 +1,13 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    FormKurs::MainForm form; //WinFormsTest - имя вашего проекта
+    Application::Run(% form);
+}
